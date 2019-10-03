e976a0c7d754c1908e38cfdb3cdbf3ce76b4fd53
whalepark <48859248+whalepark@users.noreply.github.com>
2019-10-03 10:07:03 -0400

    Update experi.sh

--- experi.sh
+++ experi.sh
@@ -1,8 +1,8 @@
-
 #!/bin/bash
 
 function run_model {
-    elapsed_time=$(nohup ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg < /dev/null 2> /dev/null | grep seconds)
+   # elapsed_time=$(nohup ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg < /dev/null 2> /dev/null | grep seconds)
+    elapsed_time=$(nohup ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg < /dev/null | grep seconds)
     FS=' ' read -r -a array <<< $elapsed_time
     log=$1','$2','${array[3]}
     echo $log
@@ -25,7 +25,7 @@ for i in $(seq 1 20); do # i=numofcurrent..
     num_of_iteration=$(echo "(20+$i-1)/$i" | bc) # floor
     for j in $(seq 1 $num_of_iteration); do
         for k in $(seq 1 $i); do
-            nohup run_model $i $(($k+$(($(($j-1))*$i)))) "linearinc" &
+            run_model $i $(($k+$(($(($j-1))*$i)))) "linearinc" &
         done
         wait
     done
